7ab124ba35f78517676e36830939ec10c87c73c1
aroig1 <aroig@arizona.edu>
2023-09-13 16:55:03 -0700

    First Commit

---
+++ Contestant.java
@@ -0,0 +1,42 @@
+public class Contestant {
+
+    private String name;
+
+    private int score;
+
+    public Contestant(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public int compareTo(Contestant o) { // Check if could pass Contestant instead of Object
+
+        if (this.score != o.getScore()) {
+            return this.score - o.getScore();
+        }
+        else {
+            return this.name.compareTo(o.getName());
+        }
+    }
+    
+    public String toString() {
+        String formated = this.name + ": " + this.score;
+        return formated;
+    }
+}
---
+++ LeaderBoard.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+import java.io.File;
+
+public class LeaderBoard {
+
+    private Contestant board[];
+
+    public LeaderBoard(int m) {
+        this.board = new Contestant[m];
+
+        File file = new File("names.txt");
+        Scanner scnr = new Scanner(file);
+        String input;
+        String name;
+        int score;
+
+        for (int i = 0; i < m; ++i) {
+            input = scnr.nextLine();
+            name = input.substring(0, input.length() - 1);
+            score = Integer.parseInt(input.substring(input.length() - 1, input.length()));
+
+            Contestant temp = new Contestant(name, score);
+            this.add(temp);
+        }
+
+    }
+
+    public void add(Contestant c) {
+        // add temp contestant declaration
+        int tempScore;
+        String tempName;
+
+        for (int i = 0; i < board.length; ++i) {
+            if ((c.compareTo(board[i]) > 0) || board[i] == null) {
+                tempScore = board[i].getScore();
+                tempName = board[i].getName();
+
+                board[i].setScore(c.getScore());
+                board[i].setName(c.getName());
+
+                Contestant temp = new Contestant(tempName, tempScore);
+                this.add(temp);
+
+                // temp = board[i];
+                // board[i] = c;
+                // this.add(temp);
+                break;
+            }
+        }
+    }
+
+    public Contestant[] finalBoard() {
+        // Not sure if this is supposed to print or not
+        for (int i = 0; i < board.length; ++i) {
+            System.out.println(board[i].toString());
+        }
+        return this.board;   
+    }
+}
